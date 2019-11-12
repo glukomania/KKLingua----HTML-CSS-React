be485030f78190d666950d8bbe50670d2bb4fa11
glukomania <glukomani@gmail.com>
2019-11-12 11:20:04 +0100

    copy of markup

---
+++ data/texts.js
@@ -0,0 +1,71 @@
+const allTexts = [
+  {
+    language: `czech`,
+    menu: {about: `O nás`, suggest: `Co nabízíme`, why: `Proč my?`, feedbacks: `Co říkají`},
+    title: `Čeština pro cizince`,
+    offersTitles: [`Individual lessons`, `Group courses`, `Corporative trainings`],
+    welcome: {
+      header: `Vítejte u nás`,
+      text: [`Jsme malá jazyková škola. Učíme češtinu pro cizince. A učíme ji moc rádi. Čeština nás baví :-) Jmenuji se Kateřina Kočová. Jsem zakladatelka jazykové školy KK Lingua. Češtinu pro cizince učím od roku 2012. Baví mě učení a baví mě cizinci, co se chtějí učit česky. Baví mě vzájemné rozšiřování jazykových i kulturních obzorů.`,
+      `Učím firemní, skupinové a individuální kurzy češtiny, organizuji společenské a turistické akce pro cizince a také pořádám taneční večery tradičních českých tanců.`],
+    },
+    about: {
+      header: `Mgr. Kateřina Kočová`,
+      text: [`Vystudovala jsem FF UK (obor Učitelství češtiny jako cizího jazyka)`,
+        `Předtím ještě Literární akademii (obor Tvůrčí psaní a publicistika)`,
+        `Učila jsem češtinu pro cizince v Centru pro integraci cizinců, Ústavu jazykové a odborné přípravy v Praze, na FF UK, v organizaci International Young Naturefriends a v jazykové škole Glossa`,
+        `Češtinu učím také ve firmách, např. v Siemens nebo v ZOOM International`,
+        `Specializuji se na netradiční metody výuky jako je například sugestopedie nebo využití dramatických prvků ve výuce`,
+        `Ve volném čase ráda tancuji, sportuji a cestuji, věnuji se kultuře a umění]`]
+    },
+    offers: {
+      header: `Co nabízíme`,
+      offers: [
+        {
+          header: `Firemní kurzy na míru`,
+          text: [`maximální časová flexibilita lektora`,
+          `lektor je vám k dispozici přímo v prostorách vaší firmy`,
+          `plán kurzu přizpůsoben konkrétním potřebám klienta`,
+          `vhodné pro náročné klienty`,
+          `Pervni nezávazná lekce ZDARMA`,
+          `tvorba individuálního studijního plánu v ceně`,
+          `cena:600 Kč/60 minut, 800 Kč/90 minut`,
+          `Začátek kdykoli. Výuka v místě práce klienta`]
+        },
+        {
+          header: `Individuální kurzy`,
+          text: [`Tempo a intenzitu výuky si volíte sami`,
+          `Lektor se vám maximálně časově přizpůsobí`,
+          `Výuka v prostorách jazykové školy případně na místě, které si sami vyberete (vaše oblíbená kavárna, bydliště)`,
+          `Bereme ohled na vaše individuální požadavky a potřeby (časové, studijní)`,
+          `Pervni nezávazná lekce ZDARMA`,
+          `Tvorba individuálního studijního plánu v ceně`,
+          `Cena 400 Kč/60 minut, 550 Kč/90 minut`,
+          `Začátek kdykoli. Výuka v místě práce klienta.`]
+        },
+        {
+          header: `Firemní kurzy na míru`,
+          text: [`maximální časová flexibilita lektora`,
+          `lektor je vám k dispozici přímo v prostorách vaší firmy`,
+          `plán kurzu přizpůsoben konkrétním potřebám klienta`,
+          `vhodné pro náročné klienty`,
+          `Pervni nezávazná lekce ZDARMA`,
+          `tvorba individuálního studijního plánu v ceně`,
+          `cena:600 Kč/60 minut, 800 Kč/90 minut`,
+          `Začátek kdykoli. Výuka v místě práce klienta`]
+        },
+      ]
+    },
+    why: {
+      header: `Proč si vybrat práve nás?`,
+      texts: {
+        cup: `Vždycky příjemná a take pohodová atmosféra při učení, kvalifikovaní lektoři`,
+        chat: `Interaktivní, názorná výuka, čeština hrou, netradiční výukové metody - rozvoj kreativity a tvořivosti`,
+        book: `Důraz na rovnoměrný rozvoj čtení, psaní, poslechu i konverzace v každé vyučovací hodině`,
+        first: `První lekce zcela zdarma - vstupní test a úvodní konverzace, doporučení ohledně volby vhodného kurzu, individuální přístup`,
+        culture: `Volný vstup na turistické a kulturní aktivity pro studenty skupinových kurzů, neformální prostředí`
+      }
+    },
+    partners: `Mezi naše klienty patri firmy jako:`
+  }
+];
